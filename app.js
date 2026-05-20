8493e9288204ae55f3a57b7af2240c4023dff553
saranaga <sarangasama@gmail.com>
2026-05-20 19:53:08 +0530

    hotfix/cors-problem-2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-	origin: '*',
+	origin: ['http://localhost:8081', 'exp://192.168.1.7:8081'],
 	credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
